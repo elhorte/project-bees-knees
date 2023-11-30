742320ceb25e36d5c33949335a311a4fe0397088
Dave Yost <dave@yost.com>
2023-11-29 18:04:59 -0800

    TestWorkList update

--- beehub/dotnet/Bees/TestWorkList/Program.fs
+++ beehub/dotnet/Bees/TestWorkList/Program.fs
@@ -12,19 +12,19 @@ let workFunc (_: CbMessage) (workId: WorkId) unregisterMe =
   match workId with WorkId id ->  printfn "WorkItem %d runs and unregisters itself." id
   unregisterMe()
 
-let printHowManyRegisteredHandlers() = printfn "%d" workList.Count
+let printHowManyRegisteredHandlers expect = printfn "actual, expect: %d %d" workList.Count expect
 
 
-printHowManyRegisteredHandlers() // 0
+printHowManyRegisteredHandlers 0 // 0
 
 handleCbMessage() // workFunc is not called bc it is not registered yet
-printHowManyRegisteredHandlers() // 0
+printHowManyRegisteredHandlers 0 // 0
 
 workList.RegisterWorkItem workFunc
-printHowManyRegisteredHandlers() // 1
+printHowManyRegisteredHandlers 1 // 1
 
 handleCbMessage() // workFunc is called and unregisters itself
-printHowManyRegisteredHandlers() // 0
+printHowManyRegisteredHandlers 0 // 0
 
 handleCbMessage() // workFunc is not called bc it is no longer registered
-printHowManyRegisteredHandlers() // 0
+printHowManyRegisteredHandlers 0 // 0
