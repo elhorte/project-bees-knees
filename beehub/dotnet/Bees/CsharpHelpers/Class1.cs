6a7d40cff18f012524c4a6f1336b3dfe7ea44b5c
daveyostcom <dave@yost.com>
2024-04-08 22:20:57 -0700

    WIP missing files added

---
+++ beehub/dotnet/Bees/CsharpHelpers/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System.Runtime.InteropServices;
+
+namespace CSharpHelpers {  
+  
+  public static class UnsafeHelpers {
+    
+    public static unsafe Span<float> CreateSpan(float* pointer, int length) {
+      return new Span<float>(pointer, length); }
+        
+    /// <summary>
+    /// Copies a block of memory using source pointer to a float array.
+    /// </summary>
+    /// <param name="sourcePtr">The pointer to a source block of memory</param>
+    /// <param name="destination">The destination float array</param>
+    /// <param name="length">The number of bytes to copy</param>
+    public static unsafe void CopyPtrToArrayAtIndex<T>(IntPtr sourcePtr, T[] destination, int index, int length)  where T : unmanaged {
+      int sizeOfT = sizeof(T);
+      int nBytes = length * sizeOfT;
+      // Make sure the destination array has enough space
+      if (index + length > destination.Length)
+        throw new ArgumentException("Destination array does not have enough space.");
+
+      // Pin the destination array
+      GCHandle gch = GCHandle.Alloc(destination, GCHandleType.Pinned);
+
+      try {
+        IntPtr destinationPtr = gch.AddrOfPinnedObject();
+
+        // Calculate the byte offset into the array where the copy will begin
+        long byteOffset = index * sizeOfT;
+
+        // Add the byte offset to the destination address
+        destinationPtr = new IntPtr(destinationPtr.ToInt64() + byteOffset);
+
+        // Perform the memory copy
+        Buffer.MemoryCopy(sourcePtr.ToPointer(), (void*)destinationPtr, nBytes, nBytes); }
+      finally {
+        // Always free the GCHandle!
+        gch.Free(); }
+
+    } } }
---
+++ beehub/dotnet/Bees/SimLib/Library.fs
@@ -0,0 +1,277 @@
+﻿module SimLib.InputStream
+
+open System
+open System.Runtime.InteropServices
+open System.Threading
+open System.Threading.Tasks
+
+open PortAudioSharp
+open BeesLib.DebugGlobals
+open BeesLib.CbMessagePool
+
+
+
+//–––––––––––––––––––––––––––––––––––––––––––––––––––
+
+let dummyInstance<'T>() =
+  System.Runtime.CompilerServices.RuntimeHelpers.GetUninitializedObject(typeof<'T>)
+  |> unbox<'T>
+
+let delayMs print ms =
+  if print then Console.Write $"\nDelay %d{ms}ms. {{"
+  (Task.Delay ms).Wait() |> ignore
+  if print then Console.Write "}"
+
+let awaitForever() = delayMs false Int32.MaxValue
+
+//––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+let paTryCatch f (exit: Exception -> unit) =
+  try
+    f()
+  with
+  | :? PortAudioException as ex -> 
+    Console.WriteLine $"PortAudioException: ErrorCode: %A{ex.ErrorCode} %s{ex.Message}"
+    exit  ex
+    raise ex
+  | ex ->
+    Console.WriteLine $"Exception: %s{ex.Message}"
+    exit  ex
+    raise ex
+
+let paTryCatchRethrow f = paTryCatch f (fun e -> ()                 )
+
+//––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+type CallbackHandoff = {
+  F            : unit -> unit
+  Semaphore    : SemaphoreSlim
+  Cts          : CancellationTokenSource
+  mutable Task : Task option } with
+ 
+  static member New f = {
+    F         = f
+    Semaphore = new SemaphoreSlim(0)
+    Cts       = new CancellationTokenSource()
+    Task      = None }
+
+  member private ch.doHandoffs() =
+    let loop() = 
+      while not ch.Cts.Token.IsCancellationRequested do
+        ch.Semaphore.WaitAsync().Wait()
+        ch.F()
+      ch.Semaphore.Dispose()
+      ch.Cts      .Dispose()
+      ch.Task <- None
+    match ch.Task with
+    | Some _ -> ()
+    | None   -> ch.Task <- Some (Task.Run loop)
+
+  member ch.Start   () = ch.doHandoffs()
+  member ch.Stop    () = ch.Cts.Cancel()
+  member ch.HandOff () = ch.Semaphore.Release() |> ignore
+
+//––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+// InputStream
+
+// Not a struct becuase it persists across calls to callback.
+type CbState = {
+  // callback args
+  mutable Input           : IntPtr
+  mutable Output          : IntPtr
+  mutable FrameCount      : uint32
+  mutable TimeInfo        : PortAudioSharp.StreamCallbackTimeInfo
+  mutable StatusFlags     : PortAudioSharp.StreamCallbackFlags
+  // callback result
+  mutable SegCur          : Seg
+  mutable SegOld          : Seg
+  mutable SeqNum          : uint64
+  mutable InputRingCopy   : IntPtr // where input was copied to
+  mutable TimeStamp       : DateTime
+  // more stuff
+  mutable IsInCallback    : bool
+  mutable NRingFrames     : int
+  mutable NGapFrames      : int
+  mutable CallbackHandoff : CallbackHandoff
+  mutable WithEcho        : bool
+  mutable WithLogging     : bool
+  TimeInfoBase            : DateTime // for getting UTC from timeInfo.inputBufferAdcTime
+  FrameSize               : int
+  RingPtr                 : IntPtr
+  DebugSimulating         : bool } with
+    
+  member this.SegOldest = if this.SegOld.Active then this.SegOld else this.SegCur
+
+
+//––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+// Copy data from the audio driver into our ring.
+
+let callback input output frameCount (timeInfo: StreamCallbackTimeInfo byref) statusFlags userDataPtr =
+  let (input : IntPtr) = input
+  let (output: IntPtr) = output
+  let handle = GCHandle.FromIntPtr(userDataPtr)
+  let cbs = handle.Target :?> CbState
+  Console.Write "."
+  Volatile.Write(&cbs.IsInCallback, true)
+  let nFrames = int frameCount
+  
+  cbs.Input        <- input
+  cbs.Output       <- output
+  cbs.FrameCount   <- frameCount // in the ”block“ to be copied
+  cbs.TimeInfo     <- timeInfo
+  cbs.StatusFlags  <- statusFlags
+  cbs.SeqNum       <- cbs.SeqNum + 1UL
+
+  if cbs.WithEcho then
+    let size = uint64 (frameCount * uint32 cbs.FrameSize)
+    Buffer.MemoryCopy(input.ToPointer(), output.ToPointer(), size, size)
+
+  let printCurAndOld msg =
+    if not cbs.DebugSimulating then () else 
+    let sCur = cbs.SegCur.Print "cur"
+    let sOld = cbs.SegOld.Print "old"
+    Console.WriteLine $"%s{sCur} %s{sOld} %s{msg}"
+  let prepSegs() =
+    // state is Empty, AtBegin, Moving, AtEnd, Chasing
+    printCurAndOld ""
+    let nextHead = cbs.SegCur.Head + nFrames
+    if nextHead <= cbs.NRingFrames then
+      // state is not AtEnd
+      // state is Empty, AtBegin, Moving, Chasing
+      // The block will fit after SegCur.Head
+      do // maybe trim SegCur.Tail
+        let nUsableFrames = cbs.NRingFrames - cbs.NGapFrames
+        cbs.SegCur.Tail <- max cbs.SegCur.Tail (nextHead - nUsableFrames)
+        // state is unchanged
+      do // maybe trim SegOld.Tail
+        if cbs.SegOld.Active then
+          // state is Chasing
+          // SegOld is active and ahead of SegCur.
+          assert (cbs.SegCur.Head < cbs.SegOld.Tail)
+          // Trim nFrames from the SegOld.Tail or if SegOld is too small, make it inactive.
+          if cbs.SegOld.NFrames > nFrames then
+            cbs.SegOld.Tail <- cbs.SegOld.Tail + nFrames
+            // state is Chasing
+          else
+            cbs.SegOld.Reset()
+            // state is AtBegin
+        // state is AtBegin, Chasing
+    else
+      // state is AtEnd
+      // The block will not fit after SegCur.Head.
+      let exchangeSegs() =
+        assert not cbs.SegOld.Active
+        let tmp = cbs.SegCur  in  cbs.SegCur <- cbs.SegOld  ;  cbs.SegOld <- tmp
+        // if SegCur.Head <> 0 then  Console.WriteLine "head != 0"
+        // assert (SegCur.Head = 0)
+        cbs.SegCur.TimeHead <- tbdDateTime
+      exchangeSegs()
+      printCurAndOld "exchanged"
+      assert (cbs.SegCur.Head = 0)
+      // SegCur starts fresh with head = 0, tail = 0.
+      // Trim away SegOld.Tail to ensure the gap.
+      cbs.SegOld.Tail <- nFrames + cbs.NGapFrames
+      // state is Chasing
+ 
+  prepSegs() // may update SegCur.Head, used by copyToRing()
+  // At this point state cannot be AtEnd.
+  // Below, after the block is appended to SegCur, the state can be AtEnd.
+//cbs.Logger.Add cbs.SeqNum cbs.TimeStamp "cb bufs=" ""
+  do
+    // Copy the block to the ring.
+    // Copy from callback data to the head of the ring and return a pointer to the copy.
+    let indexToVoidptr index  : voidptr =
+      let indexByteOffset = index * cbs.FrameSize
+      let intPtr = cbs.RingPtr + (IntPtr indexByteOffset)
+      intPtr.ToPointer()
+    let srcPtr = input.ToPointer()
+    let dstPtr = indexToVoidptr cbs.SegCur.Head
+    let size   = int64 (nFrames * cbs.FrameSize)
+    Buffer.MemoryCopy(srcPtr, dstPtr, size, size)
+    cbs.InputRingCopy <- IntPtr dstPtr
+    let timeHead = cbs.TimeInfoBase + TimeSpan.FromSeconds timeInfo.inputBufferAdcTime
+    cbs.SegCur.AdvanceHead nFrames timeHead
+  cbs.CallbackHandoff.HandOff()
+  Volatile.Write(&cbs.IsInCallback, false)
+  PortAudioSharp.StreamCallbackResult.Continue
+
+
+//––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+// InputStream
+
+// initPortAudio() must be called before this constructor.
+type InputStream( sampleRate       : int              ,
+                  frameSize        : int              ,
+                  inputParameters  : StreamParameters ,
+                  outputParameters : StreamParameters ) =
+
+  let nRingFrames       = 37
+  let nGapFrames        = 4
+  let nRingBytes        = int nRingFrames * frameSize
+  let startTime         = DateTime.UtcNow
+
+  let cbState = {
+    // callback args  
+    Input           = IntPtr.Zero
+    Output          = IntPtr.Zero
+    FrameCount      = 0u
+    TimeInfo        = PortAudioSharp.StreamCallbackTimeInfo()
+    StatusFlags     = PortAudioSharp.StreamCallbackFlags()
+    // callback result
+    SegCur          = Seg.NewEmpty nRingFrames sampleRate
+    SegOld          = Seg.NewEmpty nRingFrames sampleRate
+    SeqNum          = 0UL
+    InputRingCopy   = IntPtr.Zero
+    TimeStamp       = DateTime.MaxValue // placeholder
+    // more stuff
+    IsInCallback    = false
+    NRingFrames     = nRingFrames
+    NGapFrames      = nGapFrames
+    CallbackHandoff = dummyInstance<CallbackHandoff>()  // tbd
+    WithEcho        = false
+    WithLogging     = false
+    TimeInfoBase    = startTime  // timeInfoBase + adcInputTime -> cbState.TimeStamp
+    FrameSize       = frameSize
+    RingPtr         = Marshal.AllocHGlobal(nRingBytes)
+//  Logger          = Logger(8000, startTime)
+    DebugSimulating = simulatingCallbacks  }
+
+  let callbackStub = PortAudioSharp.Stream.Callback(
+    // The intermediate lambda here is required to avoid a compiler error.
+    fun        input output frameCount  timeInfo statusFlags userDataPtr ->
+      callback input output frameCount &timeInfo statusFlags userDataPtr )
+  let paStream = paTryCatchRethrow (fun () -> new PortAudioSharp.Stream(inParams        = Nullable<_>(inputParameters )        ,
+                                                                        outParams       = Nullable<_>(outputParameters)        ,
+                                                                        sampleRate      = sampleRate                           ,
+                                                                        framesPerBuffer = PortAudio.FramesPerBufferUnspecified ,
+                                                                        streamFlags     = StreamFlags.ClipOff                  ,
+                                                                        callback        = callbackStub                         ,
+                                                                        userData        = cbState                              ) )
+  
+  member val CbState  = cbState 
+  member val PaStream = paStream
+  
+  member this.Start() =
+    this.CbState.CallbackHandoff <- CallbackHandoff.New this.AfterCallback
+    this.CbState.CallbackHandoff.Start()
+    if this.CbState.DebugSimulating then ()
+    else
+    paTryCatchRethrow(fun() -> this.PaStream.Start() )
+  
+  member this.Stop () =
+    this.CbState.CallbackHandoff.Stop ()
+    if this.CbState.DebugSimulating then ()
+    else
+    paTryCatchRethrow(fun() -> this.PaStream.Stop () )
+
+  member this.Callback(input, output, frameCount, timeInfo: StreamCallbackTimeInfo byref, statusFlags, userDataPtr) =
+              callback input  output  frameCount &timeInfo                                statusFlags  userDataPtr
+
+  member this.AfterCallback() =
+    Console.Write ","
+
+  
+  interface IDisposable with
+    member this.Dispose() =
+      System.Console.WriteLine("Disposing inputStream")
+      this.PaStream.Dispose()
